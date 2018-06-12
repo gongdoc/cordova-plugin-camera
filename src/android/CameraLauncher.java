e0013ca436f8cd174ea14f3eb00eb2d7b95a3ca8
Sangwon Lee <emicree@gmail.com>
2018-06-12 14:23:47 +0900

    이미지 선택 창 UI 수정 테스트

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -386,16 +386,14 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                 intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, croppedUri);
             } else {
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                /*
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                
                 if (srcType == SAVEDPHOTOALBUM && this.cordova != null) {
 					this.cordova.startActivityForResult((CordovaPlugin) this,
-						Intent.createChooser(intent, new String(title)),
-						(srcType + 1) * 16 + returnType + 1);
+						Intent.createChooser(intent, new String(title)), 1);
+						// (srcType + 1) * 16 + returnType + 1);
 	                return;
-                }
-                
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                */
+                }                
             }
         } else if (this.mediaType == VIDEO) {
             intent.setType("video/*");
